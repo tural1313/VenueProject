6ddb440213396bdd9cfd4c76f2ae53aafdd988c6
tural1313 <ektural13@gmail.com>
2025-06-18 21:42:09 +0400

    Əlavələr tetbiq olundu

--- venueproject/settings.py
+++ venueproject/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'venueapp',
     'tinymce',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -137,3 +139,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ]
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
